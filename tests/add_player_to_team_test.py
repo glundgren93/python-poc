35be5de1b6b36c66a768050e681e2f83c32b2c5f
Gabriel Lundgren <gabriel.lundgren@outsystems.com>
2023-02-10 14:05:51 +0100

    renaming stuff

--- tests/add_player_to_team_test.py
+++ tests/add_player_to_team_test.py
@@ -6,7 +6,7 @@ from src.FakeTeamData import FakeTeamData
 from src.FakeTeamPlayerData import FakeTeamPlayerData
 
 
-def test_add_user_to_team():
+def test_add_player_to_team():
     team_id = 1
     player_id = 1
 
@@ -14,10 +14,10 @@ def test_add_user_to_team():
     team_fake_data = FakeTeamData()
     team_player_fake_data = FakeTeamPlayerData()
 
-    add_user_to_team = AddPlayerToTeam(
+    add_player_to_team = AddPlayerToTeam(
         player_fake_data, team_fake_data, team_player_fake_data
     )
-    add_user_to_team.execute(team_id, player_id)
+    add_player_to_team.execute(team_id, player_id)
 
     players = team_player_fake_data.get_players_from_team(team_id)
     teams = team_player_fake_data.get_teams_from_player(player_id)
@@ -35,11 +35,11 @@ def test_add_multiple_players_to_team():
     team_fake_data = FakeTeamData()
     team_player_fake_data = FakeTeamPlayerData()
 
-    add_user_to_team = AddPlayerToTeam(
+    add_player_to_team = AddPlayerToTeam(
         player_fake_data, team_fake_data, team_player_fake_data
     )
-    add_user_to_team.execute(team_id, player_id)
-    add_user_to_team.execute(team_id, player_to_be_added_id)
+    add_player_to_team.execute(team_id, player_id)
+    add_player_to_team.execute(team_id, player_to_be_added_id)
 
     team_players_amount = team_player_fake_data.get_players_from_team(team_id)
     player_teams_amount = team_player_fake_data.get_teams_from_player(player_id)
@@ -62,12 +62,12 @@ def test_add_player_to_multiple_teams():
     team_fake_data = FakeTeamData()
     team_player_fake_data = FakeTeamPlayerData()
 
-    add_user_to_team = AddPlayerToTeam(
+    add_player_to_team = AddPlayerToTeam(
         player_fake_data, team_fake_data, team_player_fake_data
     )
-    add_user_to_team.execute(team_id, player_id)
-    add_user_to_team.execute(second_team_id, player_id)
-    add_user_to_team.execute(third_team_id, player_id)
+    add_player_to_team.execute(team_id, player_id)
+    add_player_to_team.execute(second_team_id, player_id)
+    add_player_to_team.execute(third_team_id, player_id)
 
     team_players_amount = team_player_fake_data.get_players_from_team(team_id)
     second_team_players_amount = team_player_fake_data.get_players_from_team(team_id)
@@ -89,10 +89,10 @@ def test_cannot_add_player_to_non_existing_team():
         team_fake_data = FakeTeamData()
         team_player_fake_data = FakeTeamPlayerData()
 
-        add_user_to_team = AddPlayerToTeam(
+        add_player_to_team = AddPlayerToTeam(
             player_fake_data, team_fake_data, team_player_fake_data
         )
-        add_user_to_team.execute(team_id, player_id)
+        add_player_to_team.execute(team_id, player_id)
 
 
 def test_cannot_add_non_existing_player_to_team():
@@ -104,7 +104,7 @@ def test_cannot_add_non_existing_player_to_team():
         team_fake_data = FakeTeamData()
         team_player_fake_data = FakeTeamPlayerData()
 
-        add_user_to_team = AddPlayerToTeam(
+        add_player_to_team = AddPlayerToTeam(
             player_fake_data, team_fake_data, team_player_fake_data
         )
-        add_user_to_team.execute(team_id, player_id)
+        add_player_to_team.execute(team_id, player_id)
